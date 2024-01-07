4e72e5896e72594f0cd154c7c74aa619aa5b17e2
LUIS GERARDO MEDINA MORENO <luis.medina2@ulatina.net>
2024-01-07 14:35:33 -0600

    update configs

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,5 @@
 export const PORT = process.env.PORT || 3000;
 
-export const CLIENT_URL = "https://aslanlm.github.io/TaskApp" || "http://localhost:5173";
+export const CLIENT_URL = "http://localhost:5173" || "https://aslanlm.github.io/TaskApp";
 
 export const TOKEN_SECRET = "some secret key";
