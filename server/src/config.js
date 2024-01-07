87e73990dfc6d570326eef5615a2258dac0a6b0d
LUIS GERARDO MEDINA MORENO <luis.medina2@ulatina.net>
2024-01-07 00:57:59 -0600

    update configs

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,5 @@
 export const PORT = process.env.PORT || 3000;
 
-export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
+export const CLIENT_URL = "http://localhost:5173";
 
 export const TOKEN_SECRET = "some secret key";
